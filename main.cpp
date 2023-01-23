dd4692c221270d553901e7c408de994592d1b5dc
neko-nyashka <anastasiatrufanova@MacBook-Air-Anastasia.local>
2023-01-23 20:30:16 +0300

    new cases are added

--- main.cpp
+++ main.cpp
@@ -4,41 +4,57 @@ template<typename type>
 int check(type *a,int n){
     int f=0;
     for(int i=0;i<n-1;++i){
-        if (a[i]<=a[i+1]){
+        if (a[i]>a[i+1]){
             if (f==0 || f==-1)
                 f=-1;
             else
             return 0;
         }
-        else if(a[i]>=a[i+1]){
+        else if(a[i]<a[i+1]){
             if (f==0 || f==1)
                 f=1;
             else
             return 0;
         }
     }
+    for(int i=0;i<n-1;++i){
+        if (a[i]==a[i+1]){
+            f==1?f=2:f=-2;
+            return f;
+        }
+    }
     return f;
 }
-// функция check возвращает -1 если массив отсортирован по убыванию, 1 если по возраствнию, 0 в противном случае
+// функция check возвращает -1 если массив отсортирован по убыванию -2 по невозрастанию, 1 если по возраствнию, 2 по неубыванию, 0 в противном случае
 void out(int x){
     if(x==0){
-        std::cout<<"the array is not sorted"<<'\n';
+        std::cout<<"массив не отсортирован"<<'\n';
     }
     if(x==1){
-        std::cout<<"the array is sorted in descending order"<<'\n';
+        std::cout<<"массив отсортирован по возрастанию"<<'\n';
     }
     if(x==-1){
-        std::cout<<"the array is sorted in ascending  order"<<'\n';
+        std::cout<<"массив отсортирован по убыванию"<<'\n';
     }
+    if(x==-2){
+        std::cout<<"массив отсортирован по невозрастанию"<<'\n';
+    }
+    if(x==2){
+        std::cout<<"массив отсортирован по неубыванию"<<'\n';
+    }
+    
 
 }
 int main(){
     const int n=5;
-    int a[n]={1,2,3,4,5};
+    // int a[n]={1,2,3,4,5};
+    int a[n]={1,1,1,1,1};
     float b[n]={5.5,4.3,2.8,2.2,1.22};
     char c[n]={'a','c','b','e','f'};
+    double d[n]={1.2, 1.2,1.2,3.0,4.5};
     out(check<int>(a,n));
     out(check<float>(b,n));
     out(check<char>(c,n));
+    out(check<double>(d,n));
     return 0;
 }

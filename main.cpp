d7cc57317acaace1d64f16f6b7067267bfd8d990
jootiee <purrrtov@ya.ru>
2023-01-31 15:59:09 +0300

    Added file closing

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int func(type array[], int n, std::string sep, std::ostream& output) {
 }
 
 int main() {
-    std::string sep = " bebebubu ";
+    std::string sep;
     std::ofstream file1("output1.txt");
 
 
@@ -23,19 +23,21 @@ int main() {
     int n = sizeof(array1) / sizeof(array1[0]);
 
     func(array1, n, sep, file1);
-
+    file1.close();
 
     sep = "\t";
     float array2[] = {1.5, 2.5, 3.123, 4.0};
     n = sizeof(array2) / sizeof(array2[0]);
     std::ofstream file2("output2.txt");
     func(array2, n, sep, file2);
+    file2.close();
 
     sep = " ";
     std::string array3[] = {"hello", "world"};
     n = sizeof(array3) / sizeof(array3[0]);
     std::ofstream file3("output3.txt");
     func(array3, n, sep, file3);
+    file2.close();
 
     return 1;
 }
